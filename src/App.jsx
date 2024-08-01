09ad628d15f74c757708e58f32297cdde10c251e
Ardian Pratama <ardnpratm@gmail.com>
2024-08-01 09:47:16 +0700

    fix layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { Toaster } from '@/components/ui/sonner';
 import { Outlet } from 'react-router-dom';
 
 export default function App() {
-  return (
-    <div className='flex min-h-dvh flex-col bg-muted/40 p-5 text-sm'>
-      <Toaster position='top-right' />
-      <div className="w-full max-w-sm mx-auto">
-      <Outlet />
-      </div>
-    </div>
-  );
+	return (
+		<div className='bg-muted/40 text-sm'>
+			<Toaster position='top-right' />
+			<div className='w-full h-dvh max-w-sm mx-auto p-5'>
+				<Outlet />
+			</div>
+		</div>
+	);
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,11 +3,16 @@ import { Mail } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
 export default function HomePage() {
-  return (
-    <Button className='m-auto w-fit' asChild>
-      <Link to='/message'>
-        <Mail className='mr-2 h-4 w-4' /> Open Messages
-      </Link>
-    </Button>
-  );
+	return (
+		<div className='h-full flex justify-center items-center'>
+			<Button
+				className='w-fit'
+				asChild
+			>
+				<Link to='/message'>
+					<Mail className='mr-2 h-4 w-4' /> Open Messages
+				</Link>
+			</Button>
+		</div>
+	);
 }
